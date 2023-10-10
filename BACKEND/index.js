c20b4d67fe351acfc82ec5e0c2c36d5498cfc9fa
brijal.kansara <brijal.kansara@trinarybits.in>
2023-10-10 15:41:21 +0530

    [ADD] sending mail to the particular person yee

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
 
-app.get("/", (req,res) => {
-    res.send("Hello World")
-})
-app.get("/about", (req, res) => {
-    res.send("About route");
+const { getTransport, getToken, getMailOptions } = require("./service");
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.listen(3000, () => console.log("Server is starting on port 3000"));
+app.post("/register", (req, res) => {
+  const { email } = req.body;
+  if (!email) res.status(500).send("Invalid Email Address");
+
+  const token = getToken(email);
+  const link = `http://localhost:7010/verify?token=${token}`;
+
+  let mailRequest = getMailOptions(email, link);
+
+  //Send mail
+  return getTransport().sendMail(mailRequest, (error) => {
+    if (error) {
+      res.status(500).send("Can't send email.");
+    } else {
+      res.status(200);
+      res.send({
+        message: `Link sent to ${email}`,
+      });
+    }
+  });
+});
+
+app.listen(5000, () => console.log("Server is starting on port 5000"));
